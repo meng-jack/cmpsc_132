96862218313261947c9efa79c5624f54dd47f155
exoad <jackmeng0814@gmail.com>
2024-09-24 17:42:51 -0400

    add

--- Recitation3 And 4/rec_3.py
+++ Recitation3 And 4/rec_3.py
@@ -16,7 +16,7 @@ class Complex:
     def conjugate(self):
         return Complex(self._real, -self._imag)
 
-    def __rmul__(self,other):
+    def __rmul__(self, other):
         return self*other
 
     def __mul__(self, other):
@@ -30,3 +30,32 @@ class Complex:
             imag_part = self._imag*other
             ans = Complex(real_part, imag_part)
             return ans
+
+
+class Real(Complex):
+
+    def __init__(self, value):
+        super().__init__(value, 0)
+
+    def __eq__(self, other):
+        ''' Returns True if other is a Real object that has the same value or if other is
+            a Complex object with _imag=0 and same value for _real, False otherwise
+
+            >>> Real(4) == Real(4)
+            True
+            >>> Real(4) == Real(4.0)
+            True
+            >>> Real(5) == Complex(5, 0)
+            True
+            >>> Real(5) == Complex(5, 12)
+            False
+            >>> Real(5) == 5.5
+            False
+        '''
+        return other._imag == self._imag and other._real == self._real if isinstance(other, Complex) else self._real == other
+
+    def __int__(self):
+        return int(self._real)
+
+    def __float__(self):
+        return float(self._real)
