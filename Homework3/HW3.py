e24d818c5fe92e8b107035102a8517aaa88cd801
exoad <jackmeng0814@gmail.com>
2024-10-30 16:51:51 -0400

    add

--- Homework3/HW3.py
+++ Homework3/HW3.py
@@ -108,6 +108,7 @@ class Calculator:
             False
             >>> x._isNumber('2.56p')
             False
+            >>> x._isNumber(-2.3333)
         '''
         try:
             float(txt)
@@ -115,7 +116,7 @@ class Calculator:
         except ValueError:
             return False
 
-    def _getPostfix(self, txt):
+    def _getPostfix(self, expr) -> str | None:
         '''
             Required: _getPostfix must create and use a Stack for expression processing
             >>> x=Calculator()
@@ -156,11 +157,95 @@ class Calculator:
 
         # YOUR CODE STARTS HERE
         postfixStack = Stack()  # method must use postfixStack to compute the postfix expression
-        pass
-
+        txt=expr
+        if not isinstance(txt,str) or txt is None or len(txt)==0:
+            return None
+        j=0
+        while j<len(txt)-1:
+            if self._isNumber(txt[j]) and txt[j+1]==" ":
+                k=j+1
+                while k<len(txt) and txt[k]==" ":
+                    k+=1
+                if k<len(txt) and self._isNumber(txt[k]):
+                    return None
+            j+=1
+        txt:str="".join(txt.strip().split())
+        #print(txt)
+        j=1
+        while j<len(txt)-1:
+            if self._isNumber(txt[j]) and (txt[j+1]=="(" or txt[j-1]==")"):
+                return None
+            j+=1
+        pemdas:dict[str,int]={"-":1,"+":1,"/":2,"*":2,"^":3}
+        pemdas_operators=pemdas.keys()
+        operands="+-/*^()"
+        items:list[str]=[]
+        curr_item:str=""
+        j:int=0
+        while j<len(txt):
+            c=txt[j]
+            if c!=" ":
+                if self._isNumber(c) or (c=="." and c!=""):
+                    curr_item+=c
+                else:
+                    if curr_item:
+                        items.append(curr_item)
+                        curr_item=""
+                    if c in operands:
+                        if c=="-" and (j==0 or txt[j-1] in operands):
+                            curr_item+=c
+                        else:
+                            items.append(c)
+                    else:
+                        return None
+            j+=1
+        # print(items)
+        if curr_item:
+            items.append(curr_item)
+        res:str=""
+        openParenthesis:int=0
+        if items[0] in pemdas or items[len(items)-1] in pemdas:
+            return None
+        for i in range(0,len(items)):
+            if openParenthesis<0:
+                return None
+            token=items[i]
+            if self._isNumber(token):
+                if (token=="0" and postfixStack.peek()=="/") or (i+1!=len(items) and (items[i+1]=="(" or self._isNumber(items[i+1]))):
+                    return None
+                res+=str(float(token))+" "
+            elif token in pemdas_operators:
+                if i+1!=len(items) and items[i+1] in pemdas:
+                    return None
+                while(not postfixStack.isEmpty() and
+                   postfixStack.peek()!="(" and
+                   token!="^" and
+                   (pemdas[postfixStack.peek()]>=pemdas[token] or
+                    pemdas[postfixStack.peek()]==pemdas[token])):
+                    res+=postfixStack.pop()+" "
+                postfixStack.push(token)
+            elif token=="(":
+                openParenthesis+=1
+                postfixStack.push(token)
+            elif token==")":
+                if i+1<len(items) and items[i+1]=="(":
+                    return None
+                while not postfixStack.isEmpty() and postfixStack.peek()!="(":
+                    res+=postfixStack.pop()+" "
+                if postfixStack.isEmpty():
+                    return None
+                postfixStack.pop()
+                openParenthesis-=1
+            else:
+                return None
+        if openParenthesis!=0:
+            return None
+        while not postfixStack.isEmpty():
+            res+=postfixStack.pop()+" "
+        return res.strip()
 
     @property
-    def calculate(self):
+    def calculate(self) -> float | None:
         '''
             calculate must call _getPostfix
             calculate must create and use a Stack to compute the final result as shown in the video lecture
@@ -224,8 +309,35 @@ class Calculator:
         calcStack = Stack()   # method must use calcStack to compute the  expression
 
         # YOUR CODE STARTS HERE
-        pass
-
+        pf=self._getPostfix(self.getExpr)
+        if pf is None:
+            return None
+        tokens=pf.split(" ")
+        operators="-+/*^"
+        # print(tokens)
+        i:int=0
+        while i<len(tokens):
+            token=tokens[i]
+            if self._isNumber(token):
+                calcStack.push(float(token))
+            elif token in operators:
+                a:float=calcStack.pop()
+                b:float=calcStack.pop()
+                if token=="^":
+                    calcStack.push(b**a)
+                elif token=="+":
+                    calcStack.push(b+a)
+                elif token=="-":
+                    calcStack.push(b-a)
+                elif token=="*":
+                    calcStack.push(b*a)
+                elif token=="/":
+                    if a==0:
+                        return None
+                    else:
+                        calcStack.push(b/a)
+            i+=1
+        return calcStack.pop()
 #=============================================== Part III ==============================================
 
 class AdvancedCalculator:
@@ -264,7 +376,7 @@ class AdvancedCalculator:
         True
     '''
     def __init__(self):
-        self.expressions = ''
+        self.expressions:str = ''
         self.states = {}
 
     def setExpression(self, expression):
@@ -284,10 +396,10 @@ class AdvancedCalculator:
             False
         '''
         # YOUR CODE STARTS HERE
-        pass
+        return isinstance(word,str) and len(word)>0 and word[0].isalpha() and word.isalnum()
 
 
-    def _replaceVariables(self, expr):
+    def _replaceVariables(self, expr)-> str | None:
         '''
             >>> C = AdvancedCalculator()
             >>> C.states = {'x1': 23.0, 'x2': 28.0}
@@ -299,25 +411,76 @@ class AdvancedCalculator:
             >>> C._replaceVariables('x2 - x1')
             '28.0 - 23.0'
         '''
-        # YOUR CODE STARTS HERE
-        pass
+        tokens=expr.split(" ")
+        j=0
+        res=""
+        while j<len(tokens):
+            token=tokens[j]
+            if token in self.states.keys():
+                res+=str(self.states[token])
+            elif self._isVariable(token) and token not in self.states.keys():
+                return None
+            else:
+                res+=str(token)
+            if j!=len(tokens)-1:
+                res+=" "
+            j+=1
+        return res
 
 
     def calculateExpressions(self):
+        '''
+            >>> C = AdvancedCalculator()
+            >>> C.setExpression('A = 1;B = A + 9;C = A + B;A = 20;D = A + B + C;return D + 2 * B')
+            >>> C.states
+            {}
+            >>> C.calculateExpressions()
+            {'A = 1': {'A': 1.0}, 'B = A + 9': {'A': 1.0, 'B': 10.0}, 'C = A + B': {'A': 1.0, 'B': 10.0, 'C': 11.0}, 'A = 20': {'A': 20.0, 'B': 10.0, 'C': 11.0}, 'D = A + B + C': {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}, '_return_': 61.0}
+            >>> C.states
+            {'A': 20.0, 'B': 10.0, 'C': 11.0, 'D': 41.0}
+        '''
         self.states = {}
         calcObj = Calculator()     # method must use calcObj to compute each expression
         # YOUR CODE STARTS HERE
-        pass
+        ans:dict[str,dict[str,str]]={}
+        self.states={}
+        expr:list[str]=self.expressions.split(";")
+        for e in expr:
+            if e.strip()[0:6]=="return":
+                r=self._replaceVariables(e.strip()[6:])
+                if r is None:
+                    self.states={}
+                    return None
+                calcObj.setExpr(r)
+                ans["_return_"]=calcObj.calculate
+            else:
+                c=e.split("=")
+                r=self._replaceVariables(c[1])
+                if r is None:
+                    self.states={}
+                    return None
+                calcObj.setExpr(r)
+                self.states[c[0].strip()]=calcObj.calculate
+                ans[e]=self.states.copy()
+        return ans
 
 
 def run_tests():
     import doctest
 
     # Run tests in all docstrings
-    #doctest.testmod(verbose=True)
-
+    doctest.testmod(verbose=True)
+    #c=Calculator()
+    #c.setExpr("2^3-3*2")
+    #print(c.calculate)
+    #print(Calculator()._getPostfix("(3)3"))
+    #print(Calculator()._getPostfix(" 2.5 +         3 * (2 + (3) * ( 5^2-2 * 3 ^ ( 2 )         ) * ( 4 ) ) * ( 2 / 8 + 2 * ( 3 - 1 /3 ) ) - 2 / 3^ 2").split(" "))
     # Run tests per function - Uncomment the next line to run doctest by function. Replace Stack with the name of the function you want to test
-    doctest.run_docstring_examples(Calculator._isNumber, globals(), name='HW3',verbose=True)
+    #doctest.run_docstring_examples(Calculator._getPostfix, globals(), name='HW3',verbose=True)
+    #doctest.run_docstring_examples(Calculator._isNumber, globals(), name="HW3", verbose=True)
+    #doctest.run_docstring_examples(Calculator.calculate, globals(), name="HW3", verbose=True)
+    #doctest.run_docstring_examples(AdvancedCalculator.calculateExpressions, globals(), name="HW3", verbose=True)
+
 
 if __name__ == "__main__":
     run_tests()
---
+++ Recitation 9/mod6.py
@@ -0,0 +1,60 @@
+class Queue:
+    '''
+        Python list implementation of a FIFO Queue
+    '''
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def __len__(self):
+        return len(self.items)
+
+
+class Graph:
+    '''
+        >>> g1 = {'B': ['E', 'C'],
+        ...       'F': [],
+        ...       'C': ['F'],
+        ...       'A': ['D', 'B'],
+        ...       'D': ['C'],
+        ...       'E': ['F']}
+        >>> g = Graph(g1)
+        >>> g.bfs('A')
+        ['A', 'B', 'D', 'C', 'E', 'F']
+
+        >>> g2 = {'Bran': ['East', 'Cap'],
+        ...       'Flor': [],
+        ...       'Cap':  ['Flor'],
+        ...       'Apr':  ['Dec', 'Bran'],
+        ...       'Dec':  ['Cap'],
+        ...       'East': ['Flor']}
+        >>> g = Graph(g2)
+        >>> g.bfs('Apr')
+        ['Apr', 'Bran', 'Dec', 'Cap', 'East', 'Flor']
+    '''
+    def __init__(self, graph_repr):
+        self.adjacency_list = graph_repr
+
+    def bfs(self, start):
+        q=Queue()
+        q.enqueue(start)
+        visited=[start]
+        while not q.isEmpty():
+            for neighbor in sorted(self.adjacency_list[q.dequeue()]):
+                if neighbor not in visited:
+                    q.enqueue(neighbor)
+                    visited.append(neighbor)
+        return visited
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
